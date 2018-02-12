f0b213f43c9f2aa58b22f22e3676c299d2ac48b2
Jim Searle <jimsearle@gmail.com>
2018-02-12 15:31:49 -0800

    rest

--- wstREST.py
+++ wstREST.py
@@ -30,7 +30,8 @@ class wstREST():
         eh = EchoRequestHandler
         eh.parent = self
         self.server  = socketserver.TCPServer(self.address, eh)
-        self.logger.info("wstREST: Running on: %s:%s" % self.server.server_address)
+        self.url     = 'http://{0}:{1}'.format(self.server.server_address)
+        self.logger.info("wstREST: Running on: {0}".format(self.url)
         self.thread  = threading.Thread(target=self.server.serve_forever)
         #t.setDaemon(True) # don't hang on exit
         self.thread.start()
